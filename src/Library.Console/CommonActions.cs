b99b62d6e162f15c28a7415311aa16c78ffed514
fotimichele315 <fotimichele315@gmail.com>
2026-03-26 19:51:12 +0100

    Add SearchBooks action to CommonActions and implement book availability check

--- src/Library.Console/CommonActions.cs
+++ src/Library.Console/CommonActions.cs
@@ -2,12 +2,13 @@ namespace Library.Console;
 
 [Flags]
 public enum CommonActions
-{
-    Repeat = 0,
-    Select = 1,
-    Quit = 2,
-    SearchPatrons = 4,
-    RenewPatronMembership = 8,
-    ReturnLoanedBook = 16,
-    ExtendLoanedBook = 32
-}
+    {
+        Repeat = 0,
+        Select = 1,
+        Quit = 2,
+        SearchPatrons = 4,
+        RenewPatronMembership = 8,
+        ReturnLoanedBook = 16,
+        ExtendLoanedBook = 32,
+        SearchBooks = 64
+    }
